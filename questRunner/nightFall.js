0390507c17cfe9374ff14dd27f574b6b76a2890e
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2025-10-23 13:58:13 +0200

    [ADD] nightfall event

---
+++ questRunner/nightFall.js
@@ -0,0 +1,63 @@
+
+const eventState = {
+    day_time: "nightfall",
+    time: "",
+    messages: []
+}
+
+const eventType = "BONFIRE";
+
+function executeNightfallEvent(quest, timeSince12)
+{
+    eventState.messages = [];
+
+    eventState.time = getTimeString(timeSince12);
+
+    eventState.messages.push(eventType + " event starts");
+    eventState.messages.push("The team spends 1 hour recollecting materials");
+    eventState.messages.push("The team lights the bonfire");
+
+    timeSince12 += 60;
+
+    eventState.messages.push("Current time is: " + getTimeString(timeSince12));
+    eventState.messages.push(eventType + " event ends");
+
+    quest.events.push(eventState);
+
+
+    return timeSince12;
+}
+
+function getTimeString(timeSince12)
+{
+    const startHour = 12;
+    const startMinutes = 0;
+
+    let hoursToAdd = Math.floor(timeSince12 / 60);
+    const minutesToAdd = timeSince12 % 60;
+
+    let minutes = startMinutes + minutesToAdd;
+
+    if(minutes >= 60)
+    {
+        hoursToAdd++;
+        minutes -= 60;
+    }
+
+    let minutesString = "";
+
+    if(minutes < 10)
+    {
+        minutesString = (`0${minutes}`);
+    }
+    else
+    {
+        minutesString = (`${minutes}`);
+    }
+
+    let hourString = `${startHour + hoursToAdd}`;
+
+    const hourToReturn = hourString + ":" + minutesString;
+
+    return hourToReturn;
+}
